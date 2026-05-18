8a1db5e3a801b6ed9de37adae4c6d35667ceb6a8
Ashik Mahmud <ashikmamud187@gmail.com>
2026-05-18 22:38:37 +0600

    feat: enhance notification handling with community socket integration and user-specific mapping

--- features/notifications/notification-realtime-sync.tsx
+++ features/notifications/notification-realtime-sync.tsx
@@ -5,11 +5,13 @@ import { useEffect } from "react";
 
 import { notificationsKeys } from "./hooks";
 import { useNamespaceSocket, useRealtimeEvent, useRealtimeManager, useRealtimeSocket } from "@/features/realtime";
+import { useSession } from "next-auth/react";
 
 export function NotificationRealtimeSync() {
+    const { data: user } = useSession();
     const queryClient = useQueryClient();
 
-    const {  getNamespaceSocket,  connected } = useRealtimeManager()
+    const { getNamespaceSocket, connected } = useRealtimeManager()
     const { socket } = useNamespaceSocket('notifications')
 
     useEffect(() => {
@@ -28,8 +30,29 @@ export function NotificationRealtimeSync() {
             })
             socket?.emit('notification:acknowledge', { message: 'Notification acknowledged', notificationId: data.id });
         })
+
+        getNamespaceSocket("community")?.on('notification:new', (data) => {
+            const myIdMapping = data.idMap.find((idMap: any) => idMap.userId === user?.user?.id);
+            queryClient.setQueriesData({ queryKey: notificationsKeys.all }, (oldData: any) => {
+                if (!oldData) return oldData;
+                return {
+                    ...oldData,
+                    data: [{
+                        id: myIdMapping?.id || data.id,
+                        ...data,
+                    }, ...oldData?.data],
+                    metadata: {
+                        ...oldData?.metadata,
+                        unreadCount: oldData?.metadata?.unreadCount ? oldData.metadata.unreadCount + 1 : 1,
+                    }
+                }
+            })
+            socket?.emit('notification:acknowledge', { message: 'Notification acknowledged', notificationId: data.id });
+        })
+
         return () => {
             getNamespaceSocket('notifications')?.off('notification:new');
+            getNamespaceSocket("community")?.off('notification:new');
         }
     }, [connected])
     return null;
